a1faf9cb24739df1ab810a29c2f35f53403d01ac
jumblesale <rob@robsollars.co.uk>
2015-07-05 08:51:41 +0100

    moved html to episodes dir

--- generate.py
+++ generate.py
@@ -1,15 +1,27 @@
 import markdown
 import os
 
-header = open('text/header.html').read()
+header = open('html/header.html').read()
+footer = open('html/footer.html').read()
+index  = open('episodes/index.html', 'w')
+
+index.write(header)
+index.write('<h1>~jumblesale plays ksp</h1>')
+index.write('<ul>')
 
 for root, dirs, files in os.walk("text"):
-    for thefile in files:
+    for i, thefile in enumerate(files):
         if(not thefile.endswith('.md')):
             continue
         print(thefile)
         with open("text/%s" % thefile) as entry:
-            html = markdown.markdown(entry.read())
-            with open("text/%s" % thefile.replace('.md', '.html'), 'w') as htmlFile:
+            title = thefile.replace('.md', '.html')
+            html  = markdown.markdown(entry.read())
+            with open("episodes/%d.html" % i, 'w') as htmlFile:
                 htmlFile.write(header)
                 htmlFile.write(html)
+                htmlFile.write(footer)
+
+                index.write('<li><a href="%d.html">%s</a></li>' % (i, title))
+
+index.write('</body></html>')
