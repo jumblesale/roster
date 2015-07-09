8083e08bcbf07f6a27e6f7f5237ca6028114203c
jumblesale <rob@robsollars.co.uk>
2015-07-09 12:15:51 +0100

    fixed sorting

--- generate.py
+++ generate.py
@@ -14,19 +14,26 @@ index.write(header)
 index.write('<h1>~jumblesale plays ksp</h1>')
 index.write('<ul>')
 
+discoveredFiles = []
+
 for root, dirs, files in os.walk("text"):
     for i, thefile in enumerate(files):
         if(not thefile.endswith('.md')):
             continue
         print(thefile)
-        with open("text/%s" % thefile) as entry:
-            title = thefile.replace('.md', '.html')
-            html  = markdown.markdown(entry.read())
-            with open("episodes/%d.html" % i, 'w') as htmlFile:
-                htmlFile.write(header)
-                htmlFile.write(html)
-                htmlFile.write(footer)
+        discoveredFiles.append(thefile)
 
-                index.write('<li><a href="%d.html">%s</a></li>' % (i, title))
+discoveredFiles.sort()
+
+for i, thefile in enumerate(discoveredFiles):
+    with open("text/%s" % thefile) as entry:
+        title = thefile.replace('.md', '.html')
+        html  = markdown.markdown(entry.read())
+        with open("episodes/%d.html" % (i + 1), 'w') as htmlFile:
+            htmlFile.write(header)
+            htmlFile.write(html)
+            htmlFile.write(footer)
+
+            index.write('<li><a href="%d.html">%s</a></li>' % (i + 1, title))
 
 index.write('</ul></body></html>')
